dbb4e316ddd892a064c0500266cf5925dfc27369
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-08 22:59:50 -0400

    seeds finished starting set up of server

--- data/seeds/002-students.js
+++ data/seeds/002-students.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('students').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('students').insert([
+        { name: 'Ben Tsao',},
+        { name: 'Brett Madrid'},
+        { name: 'Brad Cantrell'},
+        { name: "Mykol Benning"}
       ]);
     });
 };
